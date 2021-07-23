d5478e97142551393afff2f4137e5bde2f5722e0
bitueboss <69850346+bitueboss@users.noreply.github.com>
2021-07-23 17:14:31 +0600

    aa

--- burger.js
+++ burger.js
@@ -26,3 +26,27 @@ btn.addEventListener('click', function () {
 
 
 });
+
+
+// using dark theme
+
+// const dark = document.getElementById("dark")
+// dark.addEventListener("click", function(){
+//   document.body.style.backgroundColor="black";
+// });
+
+
+// let colors =["yellow","red","green","blue","lime","orange"]
+// let hexCode=["0","1","2","3","4","5","6","A","B","C","D","E","F"]
+
+
+
+
+// const dark =document.getElementById("dark");
+// dark.addEventListener("click",function(){
+//     const num =rand(0,colors.length-1);
+   
+//     document.body.style.backgroundColor=colors[num];
+    
+    
+// })
